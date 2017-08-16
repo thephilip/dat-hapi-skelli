e1c4cf69b9e9cd09d568078c3c32535d2f5fc277
Philip Smith <thephilipsmith@gmail.com>
2017-08-16 15:24:36 -0400

    patch: housekeeping

--- src/server.js
+++ src/server.js
@@ -5,15 +5,19 @@ import conf from './conf'
 
 const server = new Hapi.Server()
 
+// configure connection
 server.connection({
 	host: conf.host,
 	port: conf.port
 })
 
+// register routes
 server.route(routes)
 
+// start server
 server.start((err) => {
 	if (err) {
+		console.error(`Error: -----------------------------\n${err}`)
 		throw err
 	}
 	console.log(`------------------------------------`)
